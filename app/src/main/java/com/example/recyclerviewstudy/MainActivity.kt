a7b431e86bc16e8628e33d59c5015b0b8c250737
leeda06 <88700168+leeda06@users.noreply.github.com>
2023-05-25 20:02:46 +0900

    2023.05.25 코틀린 방과후 수업 RecyclerView 학습 -> 목록을 표시하기위해서 사용
    목록을 표시하기 위해 가장 중요한 준비물
    - 뷰를 레이아웃에 정의
    - 목록에 필요한 데이터
    - 각개별 항목을 표시할 레이아웃
    - (*) Adapter 클래스 구현
    
    1. contact data클래스에 이름, 전화번호가 저장되어 있고, 이 두정보를 출력하는 RecyclerView구현
    
    2. 지금까지 작성한 코드 복습

---
+++ app/src/main/java/com/example/recyclerviewstudy/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.recyclerviewstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter(val data: List<String>)
+    : RecyclerView.Adapter<ListAdapter.ItemViewHolder>() {
+    class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val flater = LayoutInflater.from(parent.context)
+        val view = LayoutInflater
+                    .from(parent.context)
+                    .inflate(viewType, parent, false)
+        return ItemViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val view = holder.view
+        val listItemText = view.findViewById<TextView>(R.id.list_item_test)
+        val item = data[position]
+        listItemText.text = item
+        // holder.view.findViewById<TextView>(R.id.list_item_text).text = data[position]
+    }
+    override fun getItemCount(): Int {
+        return data.size
+    }
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.list_item
+    }
+}
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val data = mutableListOf<String>()
+        data.add("apple")
+        data.add("banana")
+        data.add("orange")
+
+        val listView = findViewById<RecyclerView>(R.id.list)
+        listView.setHasFixedSize(false)
+        listView.layoutManager = LinearLayoutManager(this)
+        listView.adapter = ListAdapter(data)
+        //listView.setAdapter(ListAdapter(data))
+
+    }
+}
